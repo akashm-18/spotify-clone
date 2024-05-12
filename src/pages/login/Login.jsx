6bd8ecf5ba3d656d3cf72144570e15bd5faeefb1
akashm-18 <akashmurugan338@gmail.com>
2024-05-12 12:00:46 +0530

    Just started

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='login'>
+      {/* Spotify Logo */}
+      <h2>Spotify</h2>
+      {/* Login with Spotify Button */}
+    </div>
+  )
+}
+
+export default Login
